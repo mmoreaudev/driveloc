1534afbb4d9ae604022ccbaf4e2ed3407a93fc70
Matéo MOREAU <contact@mateomoreau.com>
2026-06-03 13:29:36 +0200

    AFFICHER LES ERREURS

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ declare(strict_types=1);
 
 $envFile = __DIR__ . '/.env';
 
+$displayErrorsFlag = strtolower((string) (getenv('DEBUG_DISPLAY_ERRORS') ?: 'false'));
+$displayErrors = in_array($displayErrorsFlag, ['1', 'true', 'yes', 'on'], true);
+ini_set('display_errors', $displayErrors ? '1' : '0');
+ini_set('display_startup_errors', $displayErrors ? '1' : '0');
+error_reporting(E_ALL);
+
 
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/config/database.php';
